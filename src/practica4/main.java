c354ad2e3dca8b05073b2481656c7657477bdac6
kramirez8808 <kramirez8808@gmail.com>
2022-10-09 20:39:14 -0600

    Commit Menu Main

--- src/practica4/main.java
+++ src/practica4/main.java
@@ -4,6 +4,9 @@
  */
 package practica4;
 
+//Librerias
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author fanta
@@ -15,6 +18,44 @@ public class main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
+        //Menu Principal
+        int opcionMenu = 0;
+
+        while (opcionMenu != 5) {
+            opcionMenu = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite la opcion que desea:\n" +
+                                                            "1. Biblioteca\n" +
+                                                            "2. Empleado\n" +
+                                                            "3. Usuario\n" +
+                                                            "4. Libro\n" +
+                                                            "5. Salir"));
+
+            //Biblioteca
+            if (opcionMenu == 1) {
+
+                //Codigo de clase y objeto Biblioteca
+
+            //Empleado
+            } else if (opcionMenu == 2) {
+
+                //Codigo de clase y objeto Empleado
+
+            //Usuario
+            } else if (opcionMenu == 3) {
+
+                //Codigo de clase y objeto Usuario
+            
+            //Libro
+            } else if (opcionMenu == 4) {
+
+                //Codigo de clase y objeto Libro
+
+            } else if (opcionMenu == 5) {
+                JOptionPane.showMessageDialog(null, "Gracias por usar el programa");
+            } else {
+                JOptionPane.showMessageDialog(null, "Opcion no valida");
+            }
+        }
     }
     
 }
