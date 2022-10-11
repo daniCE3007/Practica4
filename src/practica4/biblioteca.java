b2922e1eb1a93356828ca94896e6b0611a8b8d8d
LeonardoCZZ <113485996+LeonardoCZZ@users.noreply.github.com>
2022-10-11 00:40:54 -0600

    Update biblioteca.java

--- src/practica4/biblioteca.java
+++ src/practica4/biblioteca.java
@@ -6,8 +6,53 @@ package practica4;
 
 /**
  *
- * @author fanta
+ * @author LeonardoCZ
  */
 public class biblioteca {
+    private String sede;
+    private String numerotelefonico;
+    private String horario;
+    private String IDbiblioteca;
+    private String inventarioLibros;
+    private String historial;
+    
+    public biblioteca(String Sede, String Numerotelefonico, String Horario, 
+            String IDBiblioteca, String InventarioLibros , String Historial){
+        this.sede = Sede;
+        this.numerotelefonico = Numerotelefonico;
+        this.horario = Horario;
+        this.IDbiblioteca = IDBiblioteca;
+        this.inventarioLibros = InventarioLibros;
+        this.historial = Historial;
+    }
+    
+    public String getSede(){
+        return sede;
+    }
+    
+    public String getNumerotelefonico(){
+        return numerotelefonico;
+    }
+    
+    public String getHorario(){
+        return horario;
+    }
+    
+    public String getIDBiblioteca(){
+        return IDbiblioteca;
+    }
+    
+    public String getInventarioLibros(){
+        return inventarioLibros;
+    }
+    
+    public String getHistorial(){
+        return historial;
+    }
+
+    
+    
+    
     
 }
+
