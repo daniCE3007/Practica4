f0d4c96dc19815dd19180385e703748b929024f9
andrewbocker <58228967+andrewbocker@users.noreply.github.com>
2022-10-10 18:26:57 -0600

    Libro.java

--- src/practica4/libro.java
+++ src/practica4/libro.java
@@ -10,4 +10,45 @@ package practica4;
  */
 public class libro {
     
+    public class libro {
+
+    private String autor;
+    private String titulo;
+    private String genero;
+    private String IDlibro;
+    private String Disponibilidad;
+
+    // Constructor de la clase
+    public libro(String Autor, String Titulo, String Genero, String ID, String Disponibilidad) {
+        this.autor = Autor;
+        this.titulo = Titulo;
+        this.genero = Genero;
+        this.IDlibro = ID;
+        this.Disponibilidad = Disponibilidad;
+    }
+
+    public libro mylibro = new libro(autor, titulo, genero, IDlibro, Disponibilidad);
+
+    // Verificar disponibilidad
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getIDlibro() {
+        return IDlibro;
+    }
+
+    public String getDisponibilidad() {
+        return Disponibilidad;
+    }
+    
 }
