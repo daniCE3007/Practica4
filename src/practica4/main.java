aa966635aa52d7cbb55fe816885183936677bc7b
kramirez8808 <kramirez8808@gmail.com>
2022-10-10 18:33:49 -0600

    Commit Main Usuario & Usuario margin fix

--- src/practica4/main.java
+++ src/practica4/main.java
@@ -23,12 +23,13 @@ public class main {
         int opcionMenu = 0;
 
         while (opcionMenu != 5) {
-            opcionMenu = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite la opcion que desea:\n" +
-                                                            "1. Biblioteca\n" +
-                                                            "2. Empleado\n" +
-                                                            "3. Usuario\n" +
-                                                            "4. Libro\n" +
-                                                            "5. Salir"));
+            opcionMenu = Integer.parseInt(JOptionPane.showInputDialog(null, 
+                    "Digite la clase que desea probar:\n" +
+                    "1. Biblioteca\n" +
+                    "2. Empleado\n" +
+                    "3. Usuario\n" +
+                    "4. Libro\n" +
+                    "5. Salir"));
 
             //Biblioteca
             if (opcionMenu == 1) {
@@ -42,8 +43,120 @@ public class main {
 
             //Usuario
             } else if (opcionMenu == 3) {
-
+                
                 //Codigo de clase y objeto Usuario
+                JOptionPane.showMessageDialog(null, "Para ingresar al menu de"
+                        + " usuario, por favor cree una nueva cuenta");
+
+                //Obtener parametros para crear usuario
+                String nombre = JOptionPane.showInputDialog("Ingrese su "
+                        + "nombre:");
+                String apellido = JOptionPane.showInputDialog("Ingrese su "
+                        + "apellido:");
+                int userID = (int) Math.round(Math.random() * 10000);
+                String usuario = JOptionPane.showInputDialog("Ingrese su "
+                        + "nombre usuario:");
+                String password = JOptionPane.showInputDialog("Ingrese su "
+                        + "contraseña:");
+                
+                //Crear objeto usuario
+                usuario MyUsuario = new usuario(nombre, apellido, userID, 
+                        usuario, password);
+                
+                //Confirmacion de creacion de usuario
+                JOptionPane.showMessageDialog(null, "Usuario creado con éxito\n"
+                        + "Nombre: " + MyUsuario.getNombre() + "\n" +
+                        "Apellido: " + MyUsuario.getApellido() + "\n" +
+                        "ID: " + MyUsuario.getUserID() + "\n" +
+                        "Usuario: " + MyUsuario.getUsuario() + "\n" +
+                        "Contraseña: " + MyUsuario.getPassword());
+                
+                //Menu de usuario
+                int opcionUser = 0;
+        
+                while (opcionUser != 7) {
+        
+                    opcionUser = Integer.parseInt(JOptionPane.showInputDialog(
+                            "Menu de Usuario\n"
+                            + "1. Solicitar libro\n"
+                            + "2. Devolver libro\n"
+                            + "3. Buscar libro\n"
+                            + "4. Solicitar Soporte Tecnico\n"
+                            + "5. Cambiar datos de usuario\n"
+                            + "6. Mostrar mis datos\n"
+                            + "7. Salir"));
+        
+                    if (opcionUser == 1) { //Solicitar libro
+                        MyUsuario.solicitarLibro(); 
+                    } else if (opcionUser == 2) { //Devolver libro
+                        MyUsuario.devolverLibro(); 
+                    } else if (opcionUser == 3) { //Buscar libro
+                        MyUsuario.buscarLibro(); 
+                    } else if (opcionUser == 4) { //Solicitar soporte tecnico
+                        MyUsuario.solicitarSoporte(); 
+                    } else if (opcionUser == 5) { //Cambiar datos de usuario
+
+                        //Menu de cambio de datos
+                        int opcionCambiar = 0;
+
+                        if (opcionCambiar != 5) {
+                            opcionCambiar = Integer.parseInt(JOptionPane.
+                                    showInputDialog("Menu de Cambio de Datos\n"
+                                            + "1. Cambiar nombre\n"
+                                            + "2. Cambiar apellido\n"
+                                            + "3. Cambiar nombre de usuario\n"
+                                            + "4. Cambiar contraseña\n"
+                                            + "5. Salir"));
+                            if (opcionCambiar == 1) {
+                                String nuevoNombre = JOptionPane.showInputDialog(
+                                        "Ingrese su nuevo nombre:");
+                                MyUsuario.setNombre(nuevoNombre);
+                                JOptionPane.showMessageDialog(null, "Nombre "
+                                        + "cambiado con éxito");
+                            } else if (opcionCambiar == 2) {
+                                String nuevoApellido = JOptionPane.showInputDialog(
+                                        "Ingrese su nuevo apellido:");
+                                MyUsuario.setApellido(nuevoApellido);
+                                JOptionPane.showMessageDialog(null, "Apellido "
+                                        + "cambiado con éxito");
+                            } else if (opcionCambiar == 3) {
+                                String nuevoUsuario = JOptionPane.showInputDialog(
+                                        "Ingrese su nuevo usuario:");
+                                MyUsuario.setUsuario(nuevoUsuario);
+                                JOptionPane.showMessageDialog(null, "Nombre de"
+                                        + " usuario cambiado con éxito");
+                            } else if (opcionCambiar == 4) {
+                                String nuevoPassword = JOptionPane.showInputDialog(
+                                        "Ingrese su nueva contraseña:");
+                                MyUsuario.setPassword(nuevoPassword);
+                                JOptionPane.showMessageDialog(null, "Contraseña "
+                                        + "cambiada con éxito");
+                            } else if (opcionCambiar == 5) {
+                                JOptionPane.showMessageDialog(null, "Saliendo "
+                                        + "del menu de cambio de datos");
+                            } else {
+                                JOptionPane.showMessageDialog(null, "Opcion "
+                                        + "invalida");
+                            }
+                        }
+        
+                    } else if (opcionUser == 6) { //Mostrar mis datos
+                        JOptionPane.showMessageDialog(null, "Nombre: "
+                                + MyUsuario.getNombre() + "\n"
+                                + "Apellido: " + MyUsuario.getApellido() + "\n"
+                                + "ID: " + MyUsuario.getUserID() + "\n"
+                                + "Usuario: " + MyUsuario.getUsuario() + "\n"
+                                + "Contraseña: " + MyUsuario.getPassword());
+
+                    } else if (opcionUser == 7) { //Salir
+                        JOptionPane.showMessageDialog(null, "Gracias por "
+                                + "usar el menu de usuario");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Opcion no valida");
+                    }
+        
+                }
+                
             
             //Libro
             } else if (opcionMenu == 4) {
@@ -51,7 +164,8 @@ public class main {
                 //Codigo de clase y objeto Libro
 
             } else if (opcionMenu == 5) {
-                JOptionPane.showMessageDialog(null, "Gracias por usar el programa");
+                JOptionPane.showMessageDialog(null, "Gracias por usar el "
+                        + "programa");
             } else {
                 JOptionPane.showMessageDialog(null, "Opcion no valida");
             }
--- src/practica4/usuario.java
+++ src/practica4/usuario.java
@@ -4,10 +4,102 @@
  */
 package practica4;
 
+//Librerias
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author fanta
  */
 public class usuario {
     
+    //Atributos
+    private String nombre;
+    private String apellido;
+    private Integer userID;
+    private String usuario;
+    private String password;
+
+    //Constructor
+    public usuario(String nombre, String apellido, Integer userID, 
+            String usuario, String password) {
+        //Inicializar atributos
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.userID = userID;
+        this.usuario = usuario;
+        this.password = password;
+    }
+
+    //Metodos GET
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    //Metodos SET
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    //Metodos de la clase
+    public void solicitarLibro() {
+        String titulo = JOptionPane.showInputDialog("Ingrese el titulo "
+                + "del libro que desea reservar:");
+        JOptionPane.showMessageDialog(null, "Reservando el libro: " + titulo
+                + "...");
+        JOptionPane.showMessageDialog(null, nombre + " " + apellido +
+                ", su solucitud del libro fue realizada con exito\n"
+                + "Un administrador la revisara en breve");
+    }
+
+    public void devolverLibro() {
+        String titulo = JOptionPane.showInputDialog("Ingrese el titulo "
+                + "del libro que desea devolver:");
+        JOptionPane.showMessageDialog(null, nombre + " " + apellido + 
+                ", el libro " + titulo + " ha sido devuelto con exito");
+    }
+    
+    public void buscarLibro() {
+        String titulo = JOptionPane.showInputDialog("Ingrese el titulo del "
+                + "libro que desea buscar:");
+        JOptionPane.showMessageDialog(null, "Buscando el libro: " 
+                + titulo + "...");
+        JOptionPane.showMessageDialog(null, nombre + " " + apellido + 
+                ", el libro " + titulo + " se encuentra disponible");
+    }
+
+    public void solicitarSoporte() {
+        JOptionPane.showMessageDialog(null, nombre + " " + apellido +
+                ", solucitud del soporte realizada con exito\n"
+                + "Un agente se comunicara con usted en breve");
+    }
+
 }
